99fb4f49bb5bfa3fc37f382789bbfdae96799119
Giovan Gentile <giovan_gentile@hotmail.com>
2014-12-17 08:03:30 -0500

    Initial Commit

---
+++ Out of this World/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Out of this World
+//
+//  Created by Giovan Gentile on 2014-12-17.
+//  Copyright (c) 2014 GG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Out of this World/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Out of this World
+//
+//  Created by Giovan Gentile on 2014-12-17.
+//  Copyright (c) 2014 GG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
