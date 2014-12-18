247cc897849ecc242dbc277d994b299a05181ff6
Giovan Gentile <giovan_gentile@hotmail.com>
2014-12-18 08:18:03 -0500

    first commit

--- Out of this World/OuterSpaceTableViewController.h
+++ Out of this World/OuterSpaceTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  OuterSpaceTableViewController.h
 //  Out of this World
 //
 //  Created by Giovan Gentile on 2014-12-17.
@@ -8,8 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface OuterSpaceTableViewController : UITableViewController
 
+@property(strong,nonatomic) NSMutableArray *planets;
 
 @end
-
